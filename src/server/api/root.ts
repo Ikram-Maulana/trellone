42f0675df82bef1dc7667a50ecf4e19300141220
Ikram Maulana <ikram075@ummi.ac.id>
2023-08-15 12:52:45 +0700

    Feat: Update Database Schema

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "@/server/api/routers/example";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -7,7 +6,7 @@ import { createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  // example: exampleRouter,
 });
 
 // export type definition of API
