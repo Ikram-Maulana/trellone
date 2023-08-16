222cc954e0b7bebeac9c695e17af675e12a4d10a
Ikram Maulana <ikram075@ummi.ac.id>
2023-08-16 14:40:00 +0700

    Feat: Configure Drag Drop Context Board Page

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,7 +1,14 @@
+import Column from "@/components/column";
 import Layout from "@/components/guest/layout";
 import { getServerAuthSession } from "@/server/auth";
+import { api } from "@/utils/api";
 import { type GetServerSideProps } from "next";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { useSession } from "next-auth/react";
+import {
+  DragDropContext,
+  type DropResult,
+  Droppable,
+} from "react-beautiful-dnd";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx);
@@ -21,21 +28,55 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 };
 
 export default function Board() {
+  const { data: sessionData } = useSession();
+  const {
+    data: board,
+    isLoading: isLoadingBoard,
+    isError: isErrorBoard,
+  } = api.todos.getBoardByStatus.useQuery(undefined, {
+    enabled: !!sessionData?.user,
+  });
+
+  const handleOnDragEnd = (result: DropResult) => {};
+
   return (
     <section id="board" className="container">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6">
+      <div className="mx-auto max-w-7xl px-4 md:px-6">
         <div className="py-12 md:py-20">
-          {/* <DragDropContext>
-            <Droppable
-              droppableId="board"
-              direction="horizontal"
-              type="column"
-            >
-              {(provided) => (
-                // Rendering All the Column
-              )}
-            </Droppable>
-          </DragDropContext> */}
+          {isLoadingBoard && <p>Loading...</p>}
+          {isErrorBoard && <p>Error</p>}
+          {!isLoadingBoard && !isErrorBoard && board && (
+            <>
+              <p>{JSON.stringify(board)}</p>
+
+              <DragDropContext onDragEnd={handleOnDragEnd}>
+                <Droppable
+                  droppableId="board"
+                  direction="horizontal"
+                  type="column"
+                >
+                  {(provided) => (
+                    <div
+                      className="mx-auto grid max-w-7xl grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3"
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                    >
+                      {Array.from(board?.columns.entries()).map(
+                        ([id, column], index) => (
+                          <Column
+                            key={id}
+                            id={id}
+                            todos={column.todos}
+                            index={index}
+                          />
+                        ),
+                      )}
+                    </div>
+                  )}
+                </Droppable>
+              </DragDropContext>
+            </>
+          )}
         </div>
       </div>
     </section>
