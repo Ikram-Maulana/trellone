d394e5c57d09ac0036a1bf9d9a211e263a25d1ae
Ikram Maulana <ikram075@ummi.ac.id>
2023-08-17 22:26:05 +0700

    Feat: Implement Zustand and Move Todo

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,6 +1,7 @@
 import Column from "@/components/column";
 import Layout from "@/components/guest/layout";
 import { getServerAuthSession } from "@/server/auth";
+import { useBoardStore } from "@/store/board-store";
 import { api } from "@/utils/api";
 import { type GetServerSideProps } from "next";
 import { useSession } from "next-auth/react";
@@ -29,53 +30,162 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 export default function Board() {
   const { data: sessionData } = useSession();
+  const { board, setBoardState } = useBoardStore();
   const {
-    data: board,
-    isLoading: isLoadingBoard,
-    isError: isErrorBoard,
+    data: boardData,
+    isLoading: isLoadingBoardData,
+    isError: isErrorBoardData,
+    refetch: refetchBoardData,
   } = api.todos.getBoardByStatus.useQuery(undefined, {
     enabled: !!sessionData?.user,
+    onSuccess: (data) => {
+      setBoardState(data);
+    },
   });
 
-  const handleOnDragEnd = (result: DropResult) => {};
+  const { mutate: moveTodo } = api.todos.moveTodo.useMutation({
+    onSuccess: async () => {
+      await refetchBoardData();
+    },
+  });
+
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (type === "column") {
+      const entries = Array.from(board?.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+
+      if (!removed) {
+        return;
+      }
+
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+
+    const columns = Array.from(board?.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    if (!startColIndex || !finishColIndex) {
+      return;
+    }
+
+    const startCol = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishCol = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    if (!startCol || !finishCol) {
+      return;
+    }
+
+    if (source.index === destination.index && startCol === finishCol) {
+      return;
+    }
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (!todoMoved) {
+      return;
+    }
+
+    if (startCol.id === finishCol.id) {
+      // Same column
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(columns);
+      newColumns.set(newCol.id, newCol);
+
+      setBoardState({
+        ...board,
+        columns: newColumns,
+      });
+    } else {
+      // Different column
+      const finishTodos = finishCol.todos;
+      finishTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(columns);
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      moveTodo({
+        todo: todoMoved,
+        columnId: finishCol.id,
+      });
+
+      setBoardState({
+        ...board,
+        columns: newColumns,
+      });
+    }
+  };
 
   return (
     <section id="board" className="container">
       <div className="mx-auto max-w-7xl px-4 md:px-6">
         <div className="py-12 md:py-20">
-          {isLoadingBoard && <p>Loading...</p>}
-          {isErrorBoard && <p>Error</p>}
-          {!isLoadingBoard && !isErrorBoard && board && (
-            <>
-              <p>{JSON.stringify(board)}</p>
+          {isLoadingBoardData && <p>Loading...</p>}
+          {isErrorBoardData && <p>Error</p>}
+          {!isLoadingBoardData && !isErrorBoardData && !boardData && (
+            <p>Empty</p>
+          )}
+          {!isLoadingBoardData && !isErrorBoardData && board && (
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+              <Droppable
+                droppableId="board"
+                direction="horizontal"
+                type="column"
+              >
+                {(provided) => (
+                  <div
+                    className="mx-auto grid max-w-7xl grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {Array.from(board?.columns.entries()).map(
+                      ([id, column], index) => (
+                        <Column
+                          key={id}
+                          id={id}
+                          todos={column.todos}
+                          index={index}
+                        />
+                      ),
+                    )}
 
-              <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable
-                  droppableId="board"
-                  direction="horizontal"
-                  type="column"
-                >
-                  {(provided) => (
-                    <div
-                      className="mx-auto grid max-w-7xl grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3"
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                    >
-                      {Array.from(board?.columns.entries()).map(
-                        ([id, column], index) => (
-                          <Column
-                            key={id}
-                            id={id}
-                            todos={column.todos}
-                            index={index}
-                          />
-                        ),
-                      )}
-                    </div>
-                  )}
-                </Droppable>
-              </DragDropContext>
-            </>
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
           )}
         </div>
       </div>
