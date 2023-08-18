b2a5779168a28ecc558bedb80d0e558ea540b9e7
Ikram Maulana <ikram075@ummi.ac.id>
2023-08-18 20:09:35 +0700

    Feat: Implement Delete Todo

--- src/components/todo-card.tsx
+++ src/components/todo-card.tsx
@@ -1,4 +1,17 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { useBoardStore } from "@/store/board-store";
+import { api } from "@/utils/api";
 import { type Todos } from "@prisma/client";
 import { CrossCircledIcon, DragHandleDots2Icon } from "@radix-ui/react-icons";
 import { type FC } from "react";
@@ -18,11 +31,22 @@ interface TodoCardProps {
 
 const TodoCard: FC<TodoCardProps> = ({
   todo,
+  index,
   id,
   innerRef,
   draggableProps,
   dragHandleProps,
 }) => {
+  const { board, setBoardState } = useBoardStore();
+  const { mutate: deleteTodo } = api.todos.deleteTodo.useMutation({
+    onSuccess: (deletedTodo) => {
+      const { todoIdx, status } = deletedTodo.data;
+      const newColumns = new Map(board.columns);
+      newColumns.get(status)?.todos.splice(todoIdx, 1);
+      setBoardState({ ...board, columns: newColumns });
+    },
+  });
+
   return (
     <div
       ref={innerRef}
@@ -34,13 +58,36 @@ const TodoCard: FC<TodoCardProps> = ({
         <DragHandleDots2Icon className="h-5 w-5 text-gray-400" />
         <p className="col-span-5">{todo.title}</p>
         <div className="col-span-2 flex justify-end">
-          <Button
-            variant="outline"
-            size="icon"
-            className="text-red-500 hover:text-red-600"
-          >
-            <CrossCircledIcon className="h-5 w-5" />
-          </Button>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="text-red-500 hover:text-red-600"
+              >
+                <CrossCircledIcon className="h-5 w-5" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your todo from the board.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={() =>
+                    deleteTodo({ todoIdx: index, todo, status: id })
+                  }
+                >
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
       </div>
 
